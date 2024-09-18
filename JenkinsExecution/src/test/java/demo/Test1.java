a21569c562a6814b998ee075815ab8ce8ceaaadd
sanjaygit1 <rathinreddy14@gmail.com>
2024-09-18 19:20:57 +0530

    commit 1

---
+++ JenkinsExecution/src/test/java/demo/Test1.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+import Generic_Utilities.BaseClass;
+
+public class Test1 extends BaseClass{
+/*On Demand
+ * On Scheduling
+ * Poll SCM
+ */
+	@Test
+	public void m1()
+	{
+		System.out.println("hello");
+
+	}
+
+}
